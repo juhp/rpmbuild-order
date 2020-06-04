592437ab71ba6141c1d22047f45949d0947bb03d
Jens Petersen <petersen@redhat.com>
2020-06-05 00:25:04 +0800

    Order: switch from [ByteString] to [String]

--- src/Distribution/RPM/Build/Order.hs
+++ src/Distribution/RPM/Build/Order.hs
@@ -3,18 +3,19 @@ module Distribution.RPM.Build.Order
    dependencySortParallel)
 where
 
+import qualified Data.ByteString.Char8 as B
 import Data.Graph.Inductive.Query.DFS (topsort', components)
 
 import Distribution.RPM.Build.Graph
 
 -- | sort packages by dependencies
-dependencySort :: [Package] -> IO [Package]
+dependencySort :: [String] -> IO [String]
 dependencySort pkgs = do
-  (graph, _) <- createGraphNodes False False Nothing pkgs []
-  return $ map package $ topsort' graph
+  (graph, _) <- createGraphNodes False False Nothing (map B.pack pkgs) []
+  return $ map (B.unpack . package) $ topsort' graph
 
 -- | dependency sort of packages in graph components
-dependencySortParallel :: [Package] -> IO [[Package]]
+dependencySortParallel :: [String] -> IO [[String]]
 dependencySortParallel pkgs = do
-  (graph, _) <- createGraphNodes False False Nothing pkgs []
-  return $ map ((map package) . topsort' . subgraph graph) (components graph)
+  (graph, _) <- createGraphNodes False False Nothing (map B.pack pkgs) []
+  return $ map ((map (B.unpack . package)) . topsort' . subgraph graph) (components graph)
