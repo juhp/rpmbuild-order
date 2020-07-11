582f77e47e4d76051a1cde390777f001049381dd
Jens Petersen <petersen@redhat.com>
2020-07-11 09:52:58 +0800

    default to parallel output and make separate a sort option
    - qualify DFS

--- src/Main.hs
+++ src/Main.hs
@@ -12,7 +12,7 @@ import Control.Applicative (
                            )
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Graph.Inductive.Graph as Graph
-import Data.Graph.Inductive.Query.DFS (xdfsWith, topsort', components)
+import qualified Data.Graph.Inductive.Query.DFS as DFS
 import Data.List
 
 #if (defined(MIN_VERSION_simple_cmd_args) && MIN_VERSION_simple_cmd_args(0,1,4))
@@ -37,38 +37,48 @@ main =
   "Sort package sources (spec files) in build dependency order" $
   subcommands
   [ Subcommand "sort" "sort packages" $
-    sortSpecFiles <$> verboseOpt <*> lenientOpt <*> parallelOpt <*> subdirOpt <*> pkgArgs
+    sortSpecFiles <$> verboseOpt <*> lenientOpt <*> componentsOpt <*> subdirOpt <*> pkgArgs
   , Subcommand "deps" "sort dependencies" $
-    depsSpecFiles False <$> verboseOpt <*> lenientOpt <*> parallelOpt <*> subdirOpt <*> pkgArgs
+    depsSpecFiles False <$> verboseOpt <*> lenientOpt <*> combineOpt <*> subdirOpt <*> pkgArgs
   , Subcommand "rdeps" "sort dependents" $
-    depsSpecFiles True <$> verboseOpt <*> lenientOpt <*> parallelOpt <*> subdirOpt <*> pkgArgs
+    depsSpecFiles True <$> verboseOpt <*> lenientOpt <*> combineOpt <*> subdirOpt <*> pkgArgs
   , Subcommand "chain" "ordered output suitable for a chain-build" $
-    chainOrderFiles <$> verboseOpt <*> lenientOpt <*> subdirOpt <*> pkgArgs
+    chainOrderFiles <$> verboseOpt <*> lenientOpt <*> combineOpt <*> subdirOpt <*> pkgArgs
   , Subcommand "leaves" "List of the top leaves of package graph" $
     leavesFiles <$> verboseOpt <*> lenientOpt <*> subdirOpt <*> pkgArgs
-  , Subcommand "separate" "List independent packages from graph" $
-    separateFiles <$> verboseOpt <*> lenientOpt <*> subdirOpt <*> pkgArgs
   ]
   where
     verboseOpt = switchWith 'v' "verbose" "Verbose output for debugging"
     lenientOpt = switchWith 'l' "lenient" "Ignore rpmspec errors"
-    parallelOpt = switchWith 'p' "parallel" "Separate independent packages"
+    combineOpt = switchWith 'c' "combine" "Combine independent packages"
     subdirOpt = optional (strOptionWith 'd' "dir" "SUBDIR" "Branch directory")
     pkgArgs = some (argumentWith str "PKG...")
+    componentsOpt =
+      flagWith' Connected 'C' "connected" "Only include connected packages" <|>
+      flagWith' Separate 's' "separated" "Only list independent packages" <|>
+      flagWith Parallel Combine 'c' "combine" "Separate independent packages"
 
-sortSpecFiles :: Bool -> Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
-sortSpecFiles verbose lenient parallel mdir pkgs = do
+data Components = Parallel | Combine | Connected | Separate
+
+sortSpecFiles :: Bool -> Bool -> Components -> Maybe FilePath -> [Package] -> IO ()
+sortSpecFiles verbose lenient components mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
-  if parallel then
-    mapM_ ((B.putStrLn . B.cons '\n' . B.unwords) . topsort' . subgraph graph) (components graph)
-    else mapM_ B.putStrLn $ topsort' graph
+  case components of
+    Parallel ->
+      mapM_ ((B.putStrLn . B.cons '\n' . B.unwords) . DFS.topsort' . subgraph graph) (DFS.components graph)
+    Combine -> (B.putStrLn . B.unwords . DFS.topsort') graph
+    Connected ->
+      mapM_ ((B.putStrLn . B.cons '\n' . B.unwords) . DFS.topsort' . subgraph graph) $ filter ((>1) . length) (DFS.components graph)
+    Separate ->
+      let independent = separatePackages graph
+      in mapM_ B.putStrLn independent
 
 depsSpecFiles :: Bool -> Bool -> Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
 depsSpecFiles rev verbose lenient parallel mdir pkgs = do
   allpkgs <- map B.pack . filter (\ f -> head f /= '.') <$> listDirectory "."
   (graph, nodes) <- createGraphNodes verbose lenient mdir allpkgs pkgs
   let direction = if rev then Graph.suc' else Graph.pre'
-  sortSpecFiles verbose lenient parallel mdir $ xdfsWith direction third nodes graph
+  sortSpecFiles verbose lenient (if parallel then Parallel else Combine) mdir $ DFS.xdfsWith direction third nodes graph
   where
     third (_, _, c, _) = c
 
@@ -80,21 +90,18 @@ listDirectory path =
   where f filename = filename /= "." && filename /= ".."
 #endif
 
--- FIXME option to separate graph components
-chainOrderFiles :: Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
-chainOrderFiles verbose lenient mdir pkgs = do
+chainOrderFiles :: Bool -> Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
+chainOrderFiles verbose lenient combine mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
-  let chain = intercalate [B.pack ":"] $ packageLayers graph
-  B.putStrLn $ B.intercalate (B.pack " ") chain
+  if combine then doChain graph
+    else mapM_ doChain $ map (subgraph graph) (DFS.components graph)
+  where
+    doChain graph =
+      let chain = intercalate [B.pack ":"] $ packageLayers graph
+      in B.putStrLn $ B.intercalate (B.pack " ") chain
 
 leavesFiles :: Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
 leavesFiles verbose lenient mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
   let leaves = packageLeaves graph
   mapM_ B.putStrLn leaves
-
-separateFiles :: Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
-separateFiles verbose lenient mdir pkgs = do
-  graph <- createGraph verbose lenient mdir pkgs
-  let independent = separatePackages graph
-  mapM_ B.putStrLn independent
