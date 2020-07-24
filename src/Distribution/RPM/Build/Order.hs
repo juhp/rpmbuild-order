f94f14ba245979426a9553c4ae41133e8c4c960e
Jens Petersen <petersen@redhat.com>
2020-07-24 15:20:30 +0800

    Order: s/String/FilePath/

--- src/Distribution/RPM/Build/Order.hs
+++ src/Distribution/RPM/Build/Order.hs
@@ -16,28 +16,28 @@ import Data.Graph.Inductive.Query.DFS (topsort', components)
 import Distribution.RPM.Build.Graph
 
 -- | sort packages by dependencies
-dependencySort :: [String] -> IO [String]
+dependencySort :: [FilePath] -> IO [FilePath]
 dependencySort pkgs = do
   topsort' <$> createGraph False False Nothing pkgs
 
 -- | dependency sort of packages in graph components
-dependencySortParallel :: [String] -> IO [[String]]
+dependencySortParallel :: [FilePath] -> IO [[FilePath]]
 dependencySortParallel pkgs = do
   graph <- createGraph False False Nothing pkgs
   return $ map (topsort' . subgraph graph) (components graph)
 
 -- | group packages in dependency layers, lowest first
-dependencyLayers :: [String] -> IO [[String]]
+dependencyLayers :: [FilePath] -> IO [[FilePath]]
 dependencyLayers pkgs = do
   graph <- createGraph False False Nothing pkgs
   return $ packageLayers graph
 
-leafPackages :: [String] -> IO [String]
+leafPackages :: [FilePath] -> IO [FilePath]
 leafPackages pkgs = do
   graph <- createGraph False False Nothing pkgs
   return $ packageLeaves graph
 
-independentPackages :: [String] -> IO [String]
+independentPackages :: [FilePath] -> IO [FilePath]
 independentPackages pkgs = do
   graph <- createGraph False False Nothing pkgs
   return $ separatePackages graph
