juhp/rpmbuild-order

commit 5e59a5fe7a1abb52565e500222e65a707cb0b20b

just use package paths in the graph directly instead of package names
This allows rpmbuild-order to return back paths not just the package names

ie it should now preserve input in output:

   spec -> spec
   dir -> dir
   pkg -> pkg  (special case of dir)

src/Distribution/RPM/Build/Graph.hs
@@ -2,9 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Distribution.RPM.Build.Graph
-  (Package,
-   SourcePackage(package),
-   createGraph,
+  (createGraph,
    createGraphNodes,
    subgraph,
    packageLayers,
@@ -43,18 +41,16 @@ listDirectory path =
   where f filename = filename /= "." && filename /= ".."
 #endif
 
-type Package = String
-
 data SourcePackage =
-   SourcePackage {
-      package :: Package,
-      dependencies :: [Package]
+  SourcePackage {
+    packagePath :: FilePath,
+    dependencies :: [FilePath]
    }
-   deriving (Show, Eq)
+   deriving Eq
 
 -- | create a directed packages dependency graph together with subset of nodes
-createGraphNodes :: Bool -> Bool -> Maybe FilePath -> [Package] -> [Package] ->
-                    IO (Gr Package (), [Graph.Node])
+createGraphNodes :: Bool -> Bool -> Maybe FilePath -> [FilePath] -> [FilePath] ->
+                    IO (Gr FilePath (), [Graph.Node])
 createGraphNodes verbose lenient mdir pkgs subset = do
   unless (all (`elem` pkgs) subset) $
     error "Packages must be in the current directory"
@@ -67,17 +63,17 @@ createGraphNodes verbose lenient mdir pkgs subset = do
     pkgNode ((i,l):ns) p = if p == l then Just i else pkgNode ns p
 
 -- | create a directed packages dependency graph
-createGraph :: Bool -> Bool -> Maybe FilePath -> [Package] -> IO (Gr Package ())
-createGraph verbose lenient mdir pkgs = do
-  specPaths <- catMaybes <$> mapM findSpec pkgs
-  let names = map takeBaseName specPaths
-  metadata <- catMaybes <$> mapM readSpecMetadata specPaths
-  let deps = mapMaybe (getDepsSrcResolved metadata) names
-      spkgs = zipWith SourcePackage names deps
+createGraph :: Bool -> Bool -> Maybe FilePath -> [FilePath] -> IO (Gr FilePath ())
+createGraph verbose lenient mdir pkgdirs = do
+  metadata <- catMaybes <$> mapM readSpecMetadata pkgdirs
+  let deps = mapMaybe (getDepsSrcResolved metadata) pkgdirs
+      spkgs = zipWith SourcePackage pkgdirs deps
       graph = getBuildGraph spkgs
   checkForCycles graph
   return graph
   where
+    -- FIXME seems too lenient for non-existent spec file
+    -- FIXME ignore dead.package instead?
     findSpec :: FilePath -> IO (Maybe FilePath)
     findSpec file =
       if takeExtension file == ".spec"
@@ -98,18 +94,22 @@ createGraph verbose lenient mdir pkgs = do
             then return $ Just f
             else return Nothing
 
-    readSpecMetadata :: FilePath -> IO (Maybe (Package,[Package],[Package]))
-    readSpecMetadata file = do
-      when verbose $ hPutStrLn stderr file
-      mcontent <- rpmspecParse lenient file
-      case mcontent of
+    readSpecMetadata :: FilePath -> IO (Maybe (FilePath,[String],[String]))
+    readSpecMetadata dir = do
+      mspec <- findSpec dir
+      case mspec of
         Nothing -> return Nothing
-        Just content ->
-          let pkg = takeBaseName file
-              (provs,brs) = extractMetadata pkg ([],[]) $ lines content
-          in return $ Just (pkg, provs, brs)
+        Just spec -> do
+          when verbose $ hPutStrLn stderr spec
+          mcontent <- rpmspecParse lenient spec
+          case mcontent of
+            Nothing -> return Nothing
+            Just content ->
+              let pkg = takeBaseName spec
+                  (provs,brs) = extractMetadata pkg ([],[]) $ lines content
+              in return $ Just (dir, provs, brs)
       where
-        extractMetadata :: Package -> ([Package],[Package]) -> [String] -> ([Package],[Package])
+        extractMetadata :: FilePath -> ([String],[String]) -> [String] -> ([String],[String])
         extractMetadata _ acc [] = acc
         extractMetadata pkg acc@(provs,brs) (l:ls) =
           let ws = words l in
@@ -126,18 +126,18 @@ createGraph verbose lenient mdir pkgs = do
                 in extractMetadata pkg (subpkg : provs, brs) ls
               _ -> extractMetadata pkg acc ls
 
-    getBuildGraph :: [SourcePackage] -> Gr Package ()
+    getBuildGraph :: [SourcePackage] -> Gr FilePath ()
     getBuildGraph srcPkgs =
        let nodes = zip [0..] srcPkgs
-           nodeDict = zip (map package srcPkgs) [0..]
+           nodeDict = zip (map packagePath srcPkgs) [0..]
            edges = do
               (srcNode,srcPkg) <- nodes
               dstNode <- mapMaybe (`lookup` nodeDict) (dependencies srcPkg)
               guard (dstNode /= srcNode)
               return (dstNode, srcNode, ())
-       in Graph.mkGraph (map (fmap package) nodes) edges
+       in Graph.mkGraph (map (fmap packagePath) nodes) edges
 
-    checkForCycles :: Monad m => Gr Package () -> m ()
+    checkForCycles :: Monad m => Gr FilePath () -> m ()
     checkForCycles graph =
        case getCycles graph of
           [] -> return ()
@@ -150,11 +150,11 @@ createGraph verbose lenient mdir pkgs = do
         getCycles =
            filter ((>= 2) . length) . scc
 
-getDepsSrcResolved :: [(Package,[Package],[Package])] -> Package -> Maybe [Package]
+getDepsSrcResolved :: [(FilePath,[String],[String])] -> FilePath -> Maybe [FilePath]
 getDepsSrcResolved metadata pkg =
   map resolveBase . thd <$> find ((== pkg) . fst3) metadata
   where
-    resolveBase :: Package -> Package
+    resolveBase :: FilePath -> FilePath
     resolveBase br =
       case mapMaybe (\ (p,provs,_) -> if br `elem` provs then Just p else Nothing) metadata of
         [] -> br
@@ -188,21 +188,21 @@ rpmspecParse lenient spec = do
     ExitFailure _ -> if lenient then return Nothing else exitFailure
     ExitSuccess -> return $ Just out
 
-packageLayers :: Gr Package () -> [[Package]]
+packageLayers :: Gr FilePath () -> [[FilePath]]
 packageLayers graph =
   if Graph.isEmpty graph then []
   else
     let layer = lowestLayer graph
     in map snd layer : packageLayers (Graph.delNodes (map fst layer) graph)
 
-lowestLayer :: Gr Package () -> [Graph.LNode Package]
+lowestLayer :: Gr FilePath () -> [Graph.LNode FilePath]
 lowestLayer graph =
   Graph.labNodes $ Graph.nfilter ((==0) . Graph.indeg graph) graph
 
-packageLeaves :: Gr Package () -> [Package]
+packageLeaves :: Gr FilePath () -> [FilePath]
 packageLeaves graph =
   map snd $ Graph.labNodes $ Graph.nfilter ((==0) . Graph.outdeg graph) graph
 
-separatePackages :: Gr Package () -> [Package]
+separatePackages :: Gr FilePath () -> [FilePath]
 separatePackages graph =
   map snd $ Graph.labNodes $ Graph.nfilter ((==0) . Graph.deg graph) graph

src/Main.hs
@@ -61,7 +61,7 @@ main =
 
 data Components = Parallel | Combine | Connected | Separate
 
-sortSpecFiles :: Bool -> Bool -> Components -> Maybe FilePath -> [Package] -> IO ()
+sortSpecFiles :: Bool -> Bool -> Components -> Maybe FilePath -> [FilePath] -> IO ()
 sortSpecFiles verbose lenient components mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
   case components of
@@ -74,7 +74,7 @@ sortSpecFiles verbose lenient components mdir pkgs = do
       let independent = separatePackages graph
       in mapM_ putStrLn independent
 
-depsSpecFiles :: Bool -> Bool -> Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
+depsSpecFiles :: Bool -> Bool -> Bool -> Bool -> Maybe FilePath -> [FilePath] -> IO ()
 depsSpecFiles rev verbose lenient parallel mdir pkgs = do
   allpkgs <- filter ((/= '.') . head) <$> listDirectory "."
   (graph, nodes) <- createGraphNodes verbose lenient mdir allpkgs pkgs
@@ -91,7 +91,7 @@ listDirectory path =
   where f filename = filename /= "." && filename /= ".."
 #endif
 
-chainOrderFiles :: Bool -> Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
+chainOrderFiles :: Bool -> Bool -> Bool -> Maybe FilePath -> [FilePath] -> IO ()
 chainOrderFiles verbose lenient combine mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
   if combine then doChain graph
@@ -101,13 +101,13 @@ chainOrderFiles verbose lenient combine mdir pkgs = do
       let chain = intercalate [":"] $ packageLayers graph
       in putStrLn $ unwords chain
 
-leavesFiles :: Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
+leavesFiles :: Bool -> Bool -> Maybe FilePath -> [FilePath] -> IO ()
 leavesFiles verbose lenient mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
   let leaves = packageLeaves graph
   mapM_ putStrLn leaves
 
-rootFiles :: Bool -> Bool -> Maybe FilePath -> [Package] -> IO ()
+rootFiles :: Bool -> Bool -> Maybe FilePath -> [FilePath] -> IO ()
 rootFiles verbose lenient mdir pkgs = do
   graph <- createGraph verbose lenient mdir pkgs
   let roots = map snd $ lowestLayer graph

commit 0df26feafb0e7bc739c03e627a3f5dedcdeeb8b0

createGraph fix critical bug introduced by 5e59a5f: only use existing pkgs
previously the passed args which may install dead.packages etc and
existing packages were getting mixed up, leading to nonsense results
when there were null package

src/Distribution/RPM/Build/Graph.hs
@@ -71,39 +71,18 @@ createGraph :: Bool -- ^ verbose
             -> Maybe FilePath -- ^ look for spec file in a subdirectory
             -> [FilePath] -- ^ package paths (directories or spec filepaths)
             -> IO (PackageGraph) -- ^ dependency graph labelled by package paths
-createGraph verbose lenient rev mdir pkgdirs = do
-  metadata <- catMaybes <$> mapM readSpecMetadata pkgdirs
-  let deps = mapMaybe (getDepsSrcResolved metadata) pkgdirs
-      spkgs = zipWith SourcePackage pkgdirs deps
+createGraph verbose lenient rev mdir paths = do
+  metadata <- catMaybes <$> mapM readSpecMetadata paths
+  let realpkgs = map fst3 metadata
+      deps = mapMaybe (getDepsSrcResolved metadata) realpkgs
+      spkgs = zipWith SourcePackage realpkgs deps
       graph = getBuildGraph spkgs
   checkForCycles graph
   return graph
   where
-    -- FIXME seems too lenient for non-existent spec file
-    -- FIXME ignore dead.package instead?
-    findSpec :: FilePath -> IO (Maybe FilePath)
-    findSpec file =
-      if takeExtension file == ".spec"
-        then checkFile file
-        else do
-        dirp <- doesDirectoryExist file
-        if dirp
-          then
-          let dir = maybe file (file </>) mdir
-              pkg = takeBaseName file in
-            checkFile $ dir </> pkg ++ ".spec"
-          else error $ "No spec file found for " ++ file
-      where
-        checkFile :: FilePath -> IO (Maybe FilePath)
-        checkFile f = do
-          e <- doesFileExist f
-          if e
-            then return $ Just f
-            else return Nothing
-
     readSpecMetadata :: FilePath -> IO (Maybe (FilePath,[String],[String]))
-    readSpecMetadata dir = do
-      mspec <- findSpec dir
+    readSpecMetadata path = do
+      mspec <- findSpec path
       case mspec of
         Nothing -> return Nothing
         Just spec -> do
@@ -114,8 +93,30 @@ createGraph verbose lenient rev mdir pkgdirs = do
             Just content ->
               let pkg = takeBaseName spec
                   (provs,brs) = extractMetadata pkg ([],[]) $ lines content
-              in return $ Just (dir, provs, brs)
+              in return $ Just (path, provs, brs)
       where
+        -- FIXME seems too lenient for non-existent spec file
+        -- FIXME ignore dead.package instead?
+        findSpec :: FilePath -> IO (Maybe FilePath)
+        findSpec file =
+          if takeExtension file == ".spec"
+            then checkFile file
+            else do
+            dirp <- doesDirectoryExist file
+            if dirp
+              then
+              let dir = maybe file (file </>) mdir
+                  pkg = takeBaseName file in
+                checkFile $ dir </> pkg ++ ".spec"
+              else error $ "No spec file found for " ++ file
+          where
+            checkFile :: FilePath -> IO (Maybe FilePath)
+            checkFile f = do
+              e <- doesFileExist f
+              if e
+                then return $ Just f
+                else return Nothing
+
         extractMetadata :: FilePath -> ([String],[String]) -> [String] -> ([String],[String])
         extractMetadata _ acc [] = acc
         extractMetadata pkg acc@(provs,brs) (l:ls) =
@@ -170,8 +171,10 @@ getDepsSrcResolved metadata pkg =
         [p] -> p
         ps -> error $ pkg ++ ": " ++ br ++ " is provided by: " ++ unwords ps
 
-    fst3 (a,_,_) = a
     thd (_,_,c) = c
+
+fst3 :: (a,b,c) -> a
+fst3 (a,_,_) = a
 
 nodeLabels :: Gr a b -> [G.Node] -> [a]
 nodeLabels graph =